bd0c82ef8526f94c2c2e506d85aeb452a4669afb
p17178463-Shridhar <p17178463@my365.dmu.ac.uk>
2020-02-05 14:57:57 +0000

    Made changes to the styles sheet as well as the index.html and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
-console.log("hello");
+// const button = document.getElementById ("text");
+//
+// button.addEventListener("dblclick", sup);
+//
+// function sup() {
+//   if ( (button.innerHTML === "Click me")) {
+//     button.innerHTML = "thanks";
+//   } else {
+//     button.innerHTML ="Click me";
+//   }
+//
+// }
+
+const button = document.getElementById ("text2");
+
+button.addEventListener("click", hello);
+let count  = 0;
+
+function hello() {
+
+  button.insertAdjacentHTML('afterend', `<p>${count}</p>`);
+  count++;
+}
