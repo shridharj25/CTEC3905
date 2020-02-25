453da2948631406168841c9e192526d22f01b598
p17178463-Shridhar <p17178463@my365.dmu.ac.uk>
2020-02-25 16:35:35 +0000

    Completed the naviagtion bar, I have also added a slideshow gallery on the homepage and chnaged the colour

--- js/scripts.js
+++ js/scripts.js
@@ -11,13 +11,41 @@
 //
 // }
 
-const button = document.getElementById ("text2");
 
-button.addEventListener("click", hello);
-let count  = 0;
+// // this is for the hamburger
+// menuButton.addEventListener("click", toggleMenu);
+//
+// function toggleMenu () {
+// console.log("clicked menu");
+// if  (myNav.classList.contains("show")){
+//   myNav.classList.remove("show");
+// } else {
+//     myNav.classList.add("show");
+// }
+//////////////////////////////////////////////
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function hello() {
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-  button.insertAdjacentHTML('afterend', `<p>${count}</p>`);
-  count++;
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
