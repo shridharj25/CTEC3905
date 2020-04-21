e3ce21d395844eed2704c7ba5b36075a3e230f04
shrid <shrid@LAPTOP-LU8O4PDS>
2020-04-21 22:12:24 +0100

    removed unwanted code from the style sheet, movies,contactus, homepage and TVNews page all have the footer fixed, added comments explaining the api code

--- js/scripts.js
+++ js/scripts.js
@@ -14,36 +14,7 @@ function toggleMenu () {
   }
 }
 
-////////////////////////////////////////////////////////////
-
-// var slideIndex = 1;
-// showSlides(slideIndex);
-//
-//
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-//
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-//
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
-
+// Code for the slideshow
 var slideIndex = 0;
 showSlides();
 
@@ -60,9 +31,6 @@ function showSlides() {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
-/////////////////////////////////////////
-
-//
